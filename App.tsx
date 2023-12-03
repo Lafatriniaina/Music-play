ed75c7620a8e4a58a7daa2d1500ccfb9131577e2
Lafatriniaina <122795779+Lafatriniaina@users.noreply.github.com>
2023-12-03 18:58:33 +0300

    Creating a music app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Playlists from './src/views/Playlists/Playlists';
+import Favoris from './src/views/Favorite/Favorite';
+import Ecouter from './src/views/Listen/Listen';
+import SearchBar from './src/views/Playlists/SearchBar';
+import PlaylistsStyles from './src/styles/PlaylistsStyles';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+   <NavigationContainer>
+     <Stack.Navigator initialRouteName="Playlists">
+       <Stack.Screen
+         name="Playlists"
+         component={Playlists}
+         options={{
+          headerTitle: '',
+           headerLeft: () => (
+             <MaterialIcons name="menu-open" size={40} color="black" style={PlaylistsStyles.menu} />
+           ),
+           headerRight: () => (
+             <SearchBar />
+           ),
+         }}
+       />
+       <Stack.Screen name="Favoris" component={Favoris} />
+       <Stack.Screen name="Ecouter" component={Ecouter} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
